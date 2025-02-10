4151b3728412d85eb74498d520a670bc2e630479
Alex <akomatoznik@gmail.com>
2025-02-10 20:14:23 +0200

    debounce

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,18 +1,18 @@
 import { searchFilter } from "./filters"
 import getData from "./getData"
+import { debounce } from "./helpers"
 import renderGoods from "./renderGoods"
 
 const search = () => {
   const searchInput = document.querySelector('.search-wrapper_input')
 
-  searchInput.addEventListener('input', (e) => {
-    const value = e.target.value
-
-    getData(value).then((data) => {
-      renderGoods(searchFilter(data, value))
+  const debounceSearch = debounce((e) => {
+    getData().then((data) => {
+      renderGoods(searchFilter(data, e.target.value))
     })
-    
   })
+
+  searchInput.addEventListener('input', debounceSearch)
 }
 
 export default search
